c102987e53d29f4d6e3a9ff4fbadec3bb4171e02
Jesse Neufeld <JN@Jesses-MacBook-Pro-2.local>
2022-11-30 14:52:54 -0800

    Setup state and like/toast logic

--- src/Content.js
+++ src/Content.js
@@ -1,14 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
+import { fetchLikedFormSubmissions, saveLikedFormSubmission, onMessage } from './service/mockServer';
 
 export default function Content() {
+  const [likes, setLikes] = useState([]);
+  const [toast, setToast] = useState(false);
+  const [displayToast, setDisplayToast] = useState(false);
+
+  onMessage((data) => setToast(data));
+  onMessage((data) => setDisplayToast(true));
+
+  const refreshList = () => {
+    fetchLikedFormSubmissions()
+    .then(data => {
+      setLikes(data.formSubmissions);
+    })
+  }
+
+  const like = () => {
+    setDisplayToast(false);
+    let formSubmission = toast;
+    formSubmission.data.liked = true;
+    saveLikedFormSubmission(formSubmission)
+    .then(data => {
+      refreshList();
+    })
+  }
+
+  useEffect(() => {
+    refreshList();
+  },[])
+
+  const Toast = () => 
+  <div>
+    {JSON.stringify(toast)}
+    <button onClick={(event) => like()}>
+      Like
+    </button>
+  </div>
+
+  const Like = props => 
+  <div>
+    {JSON.stringify(props.data)}
+  </div>
+
   return (
     <Box sx={{marginTop: 3}}>
+      {displayToast && 
+      <div>
+        <Toast/>
+      </div>}
       <Typography variant="h4">Liked Form Submissions</Typography>
 
       <Typography variant="body1" sx={{fontStyle: 'italic', marginTop: 1}}>
-        TODO: List of liked submissions here (delete this line)
+        {likes && likes.map((element) => <Like data={element} key={element.id} />)}
       </Typography>
     </Box>
   );
