36df54814310430f85d4cc271d1898feca954293
Jesse Neufeld <JN@Jesses-MacBook-Pro-2.local>
2022-12-01 16:41:14 -0800

    add test if toast displays

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
-import { screen, render } from '@testing-library/react';
+import { screen, render, cleanup, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 import App from './App';
+
+import { fetchLikedFormSubmissions, saveLikedFormSubmission, onMessage } from './service/mockServer';
+
+afterEach(() => cleanup());
 
 test('renders header text', () => {
   render(<App />);
@@ -8,3 +13,15 @@ test('renders header text', () => {
   const heading  = screen.getByRole('heading', { name: /toast exercise/i});
   expect(heading).toBeInTheDocument();
 });
+
+test('display toast', () => {
+  render(<App />);
+
+  const button = screen.getByRole('button', { name: /new submission/i} );
+  expect(button).toBeInTheDocument();
+  expect(screen.queryByRole('alert')).toBeNull();
+  fireEvent.click(button);
+  
+  const toast = screen.getByRole('alert');
+  expect(toast).toBeInTheDocument();
+})
